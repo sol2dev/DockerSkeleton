bf32df116fa6298ef6cd9e1416b872a34087cf9a
andreyv <andreyv@templatemonster.me>
2017-07-06 15:31:14 +0300

    Improved shell scripts

--- reinstall.sh
+++ reinstall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 docker-compose stop
-docker-compose rm -fv nginx
-docker-compose rm -fv fpm
-docker-compose rm -fv database
+docker-compose rm -fva
 
 docker network rm custom-network
 docker network create --driver bridge --subnet 172.19.0.0/24 custom-network
--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec -it dockerskeleton_nginx_1 bash
+echo -e "Connecting to \033[1;32m${1:-nginx}\033[0m container..."
+docker-compose run --rm ${1:-nginx} bash
